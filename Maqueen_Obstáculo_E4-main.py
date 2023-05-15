10ba7afc9b4a036a7dc976968a05dd2f56742190
Gumball <114919501+ackerman16@users.noreply.github.com>
2023-05-15 13:45:21 +0200

    Update Maqueen_Obstáculo_E4-main.py

--- Maqueen_Obstáculo_E4-main.py
+++ Maqueen_Obstáculo_E4-main.py
@@ -8,8 +8,9 @@ for i in range(0,2):
 robot.girar_izquierda()
 robot.mover_celda()
 robot.girar_derecha()
-for i in range(0,5):
+for i in range(0,6):
     robot.mover_celda()
 robot.girar_derecha()
 robot.mover_celda()
+robot.girar_izquierda
 robot.motor_stop_all()
